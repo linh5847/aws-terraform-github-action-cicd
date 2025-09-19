f9605b639bd7483b2f706412ad7923b95a72eae1
Linh Nguyen <linh.nguyen9@nhs.net>
2025-09-19 13:46:55 +0100

    run TFLinit, TFSec, Checkov

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -20,19 +20,19 @@ resource "aws_vpc" "vpc" {
 resource "aws_default_security_group" "default" {
   vpc_id = aws_vpc.vpc[0].id
 
-  ingress {
-    protocol  = "-1"
-    self      = true
-    from_port = 0
-    to_port   = 0
-  }
+  # ingress {
+  #   protocol  = "-1"
+  #   self      = true
+  #   from_port = 0
+  #   to_port   = 0
+  # }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # egress {
+  #   from_port   = 0
+  #   to_port     = 0
+  #   protocol    = "-1"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
 
   tags = merge(
     {
