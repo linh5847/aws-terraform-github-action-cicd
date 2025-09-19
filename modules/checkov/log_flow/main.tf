815f7f53c29a88a97e99905c15ac1a5f8f7fc2d9
Linh Nguyen <linh.nguyen9@nhs.net>
2025-09-19 13:18:39 +0100

    run TFLinit, TFSec, Checkov

--- modules/checkov/log_flow/main.tf
+++ modules/checkov/log_flow/main.tf
@@ -1,9 +1,14 @@
 resource "aws_flow_log" "checkov_log_flow" {
   count = var.enabled == true ? 1 : 0
-  #iam_role_arn    = "arn"
-  log_destination = "log"
-  traffic_type    = "ALL"
-  vpc_id          = var.vpc_id
+  #iam_role_arn    = "arn:aws:s3:::vntechsol"
+  log_destination      = "arn:aws:s3:::vntechsol"
+  log_destination_type = "s3"
+  traffic_type         = "ALL"
+  vpc_id               = var.vpc_id
+  destination_options {
+    file_format        = "parquet"
+    per_hour_partition = true
+  }
 
   tags = merge(
     {
