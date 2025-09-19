05aaaeff9975a180fe4c35091a3bb8d3a3423e60
Linh Nguyen <linh.nguyen9@nhs.net>
2025-09-19 12:36:46 +0100

    run TFLinit, TFSec, Checkov

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -15,3 +15,29 @@ resource "aws_vpc" "vpc" {
     create_before_destroy = true
   }
 }
+
+# checkov requirement on github ci/cd = https://docs.prismacloud.io/en/enterprise-edition/policy-reference/aws-policies/aws-networking-policies/networking-4
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.vpc[0].id
+
+  ingress {
+    protocol  = "-1"
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(
+    {
+      Name = format("%s", "default_security_group")
+    },
+    var.additional_tags
+  )
+}
